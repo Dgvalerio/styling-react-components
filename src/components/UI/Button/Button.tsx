682f4d3711d0c48aaf552a0126482f40dedf732f
Davi Gonçalves Valério <al.davigvalerio@gmail.com>
2021-07-14 12:16:14 -0300

    Class 79 - CSS Modules

--- src/components/UI/Button/Button.tsx
+++ src/components/UI/Button/Button.tsx
@@ -1,29 +1,21 @@
-import styled from 'styled-components';
+import React from 'react';
 
-const Button = styled.button`
-  font: inherit;
-  padding: 0.5rem 1.5rem;
-  border: 1px solid #8b005d;
-  color: white;
-  background: #8b005d;
-  box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
-  cursor: pointer;
-  width: 100%;
+import styles from './Button.module.css';
 
-  @media (min-width: 768px) {
-    width: auto;
-  }
-
-  &:focus {
-    outline: none;
-  }
-
-  &:hover,
-  &:active {
-    background: #ac0e77;
-    border-color: #ac0e77;
-    box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
-  }
-`;
+const Button = ({
+  type,
+  onClick,
+  children,
+}: {
+  type: 'button' | 'submit' | 'reset' | undefined;
+  // eslint-disable-next-line react/require-default-props
+  onClick?: () => void;
+  children: any;
+}): JSX.Element => (
+  // eslint-disable-next-line react/button-has-type
+  <button type={type} className={styles.button} onClick={onClick}>
+    {children}
+  </button>
+);
 
 export default Button;
