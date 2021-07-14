666677b488b73908f2b22751cc908fd90e6f0a09
Davi Gonçalves Valério <al.davigvalerio@gmail.com>
2021-07-14 11:34:59 -0300

    Class 74

--- src/components/CourseGoals/CourseInput/CourseInput.tsx
+++ src/components/CourseGoals/CourseInput/CourseInput.tsx
@@ -9,21 +9,33 @@ const CourseInput = ({
   onAddGoal: (value: any) => void;
 }): JSX.Element => {
   const [enteredValue, setEnteredValue] = useState('');
+  const [isValid, setIsValid] = useState(true);
 
   const goalInputChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
+    setIsValid(event.target.value.length > 0);
     setEnteredValue(event.target.value);
   };
 
   const formSubmitHandler = (event: FormEvent) => {
     event.preventDefault();
+    if (enteredValue.trim().length === 0) {
+      return;
+    }
     onAddGoal(enteredValue);
   };
 
   return (
     <form onSubmit={formSubmitHandler}>
       <div className="form-control">
-        <label>Course Goal</label>
-        <input type="text" onChange={goalInputChangeHandler} />
+        <label style={{ color: !isValid ? 'red' : 'black' }}>Course Goal</label>
+        <input
+          style={{
+            borderColor: !isValid ? 'red' : '#ccc',
+            backgroundColor: !isValid ? 'salmon' : 'transparent',
+          }}
+          type="text"
+          onChange={goalInputChangeHandler}
+        />
       </div>
       <Button type="submit">Add Goal</Button>
     </form>
