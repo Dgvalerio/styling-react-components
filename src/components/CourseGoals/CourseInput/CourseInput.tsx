7fd5585f3a0fd225b0b3f4fa620596b47180e9c9
Davi Gonçalves Valério <al.davigvalerio@gmail.com>
2021-07-14 12:22:14 -0300

    Class 80 - Dynamic CSS Modules

--- src/components/CourseGoals/CourseInput/CourseInput.tsx
+++ src/components/CourseGoals/CourseInput/CourseInput.tsx
@@ -1,35 +1,7 @@
 import React, { ChangeEvent, FormEvent, useState } from 'react';
 
-import styled from 'styled-components';
-
 import Button from '../../UI/Button/Button';
-
-const FormControl = styled.div<{ invalid: boolean }>`
-  margin: 0.5rem 0;
-
-  label {
-    font-weight: bold;
-    display: block;
-    margin-bottom: 0.5rem;
-    color: ${({ invalid }) => (invalid ? 'red' : 'black')};
-  }
-
-  input {
-    display: block;
-    width: 100%;
-    border: 1px solid ${({ invalid }) => (invalid ? 'red' : '#ccc')};
-    background-color: ${({ invalid }) => (invalid ? 'ffd7d7' : 'transparent')};
-    font: inherit;
-    line-height: 1.5rem;
-    padding: 0 0.25rem;
-  }
-
-  input:focus {
-    outline: none;
-    background: #fad0ec;
-    border-color: #8b005d;
-  }
-`;
+import styles from './CourseInput.module.css';
 
 const CourseInput = ({
   onAddGoal,
@@ -54,10 +26,10 @@ const CourseInput = ({
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <FormControl invalid={!isValid}>
+      <div className={`${styles.formControl} ${!isValid && styles.invalid}`}>
         <label>Course Goal</label>
         <input type="text" onChange={goalInputChangeHandler} />
-      </FormControl>
+      </div>
       <Button type="submit">Add Goal</Button>
     </form>
   );
